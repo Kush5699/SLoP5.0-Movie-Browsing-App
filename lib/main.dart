1dee4afa8fd469cbc3d8a375623e52a88dd82e7c
Kush5699 <kushp756@gmail.com>
2025-10-16 03:13:25 +0530

    feature : implemented Firebase auth (login, signup, logout) and user favorites

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_browsing_app/screens/home_screen.dart';
+import 'package:movie_browsing_app/screens/login_screen.dart';
+import 'package:movie_browsing_app/screens/signup_screen.dart';
 import 'screens/movie_detail_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+
 
   await dotenv.load(fileName: ".env");
   runApp(const MovieBrowsingApp());
@@ -25,10 +38,33 @@ class MovieBrowsingApp extends StatelessWidget {
           foregroundColor: Colors.white,
         ),
       ),
-      home: HomeScreen(), 
+      home: AuthGate(),
       routes: {
         '/movie-detail': (context) => MovieDetailScreen(),
       },
     );
   }
+}
+
+
+class AuthGate extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+
+    return StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder:(context,snap){
+          if (snap.connectionState==ConnectionState.waiting){
+            return const Scaffold(
+                body: Center(child: CircularProgressIndicator()));
+          }
+          if (snap.hasData){
+            return const HomeScreen();
+          }
+          return const LoginScreen();
+        }
+    );
+  }
+
 }
